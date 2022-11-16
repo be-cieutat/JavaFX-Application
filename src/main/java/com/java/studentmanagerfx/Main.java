b41d67eda27e8e523738b979536dd5810fbba9d3
pacom <pacome.manceaux@gmail.com>
2022-11-16 19:46:21 +0100

    GG

--- src/main/java/com/java/studentmanagerfx/Main.java
+++ src/main/java/com/java/studentmanagerfx/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("student.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
         stage.setTitle("StudentManagerFX");
         stage.setScene(scene);
         stage.show();
